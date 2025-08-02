670b92756f55de641410ad7bf86a1c04efb686f4
Hermitsfool <capt_shane@yahoo.com>
2025-08-02 08:18:54 -0500

    Update asteroidfield.py

--- asteroidfield.py
+++ asteroidfield.py
@@ -1,24 +1,51 @@
-import pygame # type: ignore
+import pygame #type:ignore
+import random #type:ignore
+from asteroid import Asteroid #type:ignore
+from constants import * #type:ignore
 
 
-# Base class for game objects
-class CircleShape(pygame.sprite.Sprite):
-    def __init__(self, x, y, radius):
-        if hasattr(self, "containers"):
-            super().__init__(self.containers)
-        else:
-            super().__init__()
-        self.position = pygame.Vector2(x, y)
-        self.velocity = pygame.Vector2(0, 0)
-        self.radius = radius
+class AsteroidField(pygame.sprite.Sprite):
+    edges = [
+        [
+            pygame.Vector2(1, 0),
+            lambda y: pygame.Vector2(-ASTEROID_MAX_RADIUS, y * SCREEN_HEIGHT),
+        ],
+        [
+            pygame.Vector2(-1, 0),
+            lambda y: pygame.Vector2(
+                SCREEN_WIDTH + ASTEROID_MAX_RADIUS, y * SCREEN_HEIGHT
+            ),
+        ],
+        [
+            pygame.Vector2(0, 1),
+            lambda x: pygame.Vector2(x * SCREEN_WIDTH, -ASTEROID_MAX_RADIUS),
+        ],
+        [
+            pygame.Vector2(0, -1),
+            lambda x: pygame.Vector2(
+                x * SCREEN_WIDTH, SCREEN_HEIGHT + ASTEROID_MAX_RADIUS
+            ),
+        ],
+    ]
 
-    def draw(self, screen):
-        # must override
-        pass
+    def __init__(self):
+        pygame.sprite.Sprite.__init__(self, self.containers)
+        self.spawn_timer = 0.0
+
+    def spawn(self, radius, position, velocity):
+        asteroid = Asteroid(position.x, position.y, radius)
+        asteroid.velocity = velocity
 
     def update(self, dt):
-        # must override
-        pass
+        self.spawn_timer += dt
+        if self.spawn_timer > ASTEROID_SPAWN_RATE:
+            self.spawn_timer = 0
 
-    def collides_with(self, other):
-        return self.position.distance_to(other.position) <= self.radius + other.radius
+            # spawn a new asteroid at a random edge
+            edge = random.choice(self.edges)
+            speed = random.randint(40, 100)
+            velocity = edge[0] * speed
+            velocity = velocity.rotate(random.randint(-30, 30))
+            position = edge[1](random.uniform(0, 1))
+            kind = random.randint(1, ASTEROID_KINDS)
+            self.spawn(ASTEROID_MIN_RADIUS * kind, position, velocity)
